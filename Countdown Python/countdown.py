05577f527ac7b01c06801ea6a9301948439e3254
Atanas Yourdanov <atanas.yordanov@ibm.com>
2024-05-21 22:52:12 +0300

    Source code directories updated and README.md modified!

--- Countdown Python/countdown.py
+++ Countdown Python/countdown.py
@@ -1,20 +1,20 @@
-import tkinter as tk
-class ExampleApp(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
-        self.label = tk.Label(self, text="", width=10)
-        self.label.pack()
-        self.remaining = 0
-        self.countdown(10)
-    def countdown(self, remaining = None):
-        if remaining is not None:
-            self.remaining = remaining
-        if self.remaining <= 0:
-            self.label.configure(text="time's up!")
-        else:
-            self.label.configure(text="%d" % self.remaining)
-            self.remaining = self.remaining - 1
-            self.after(1000, self.countdown)
-if __name__ == "__main__":
-    app = ExampleApp()
-    app.mainloop()
+# import the time module 
+import time 
+
+# define the countdown func. 
+def countdown(t): 
+	
+	while t: 
+		mins, secs = divmod(t, 60) 
+		timer = '{:02d}:{:02d}'.format(mins, secs) 
+		print(timer, end="\r") 
+		time.sleep(1) 
+		t -= 1
+	
+	print('Boooom!') 
+
+# input time in seconds 
+t = input("Enter the time in seconds: ") 
+
+# function call 
+countdown(int(t))
